8e35a0604670701f8c2546be20ecd5333bf90591
Uriel Ortega <urielortega2509@gmail.com>
2023-04-14 07:39:57 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Uriel Ortega on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
